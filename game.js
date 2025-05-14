017593bae84b46fc13542c74f7d55d845f0f2356
Adarsh Vinayak <64320422+adarshvinayak@users.noreply.github.com>
2025-05-15 00:22:36 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 // Game constants
 
-const GRAVITY = -0.1;
+//const GRAVITY = -0.1;
 
-const FLAP_FORCE = -5;
+//const FLAP_FORCE = -5;
 
 const PIPE_WIDTH = 100;
 
@@ -33,13 +33,15 @@ console.log(isMobile)
 
 // Adjust constants for mobile
 
-let MOBILE_SCALE = isMobile ? 0.6 : 1;
+let MOBILE_SCALE = isMobile ? 5 : 1;
 
 let MOBILE_PIPE_GAP = isMobile ? 250 : 200; // Larger gap on mobile
 
-let MOBILE_PIPE_SPEED = isMobile ? 1.5 : 2; // Slower speed on mobile
+let MOBILE_PIPE_SPEED = isMobile ? 4 : 2; // Slower speed on mobile
 
+let GRAVITY = isMobile? 1 : -0.1;
 
+let FLAP_FORCE = isMobile? 9 : -5;
 
 // Mutable game settings
 
